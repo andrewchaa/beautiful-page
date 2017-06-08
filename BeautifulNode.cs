bc7ff8e8e5b2d045e0554a44e9c17b24e074bc83
andrew <andrew.yh.chaa@gmail.com>
2017-06-08 15:12:46 +0100

    check if nodes are empty

--- BeautifulNode.cs
+++ BeautifulNode.cs
@@ -13,8 +13,11 @@ namespace BeautifulWeb
 
         public string ToText()
         {
-            return HtmlNode.InnerText.Trim();
+            return HtmlNode?.InnerText.Trim();
         }
 
+        public string Href => HtmlNode?.GetAttributeValue("href", string.Empty);
+        public string InnerText => HtmlNode?.InnerText.Trim();
+        public bool HasValue => HtmlNode != null;
     }
 }
--- BeautifulPage.cs
+++ BeautifulPage.cs
@@ -6,32 +6,37 @@ namespace BeautifulWeb
 {
     public class BeautifulPage
     {
-        private readonly HtmlDocument _htmlDocument;
+        private readonly HtmlNode _htmlNode;
 
         public BeautifulPage(string pageContent)
         {
-            _htmlDocument = new HtmlDocument();
-            _htmlDocument.LoadHtml(pageContent);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(pageContent);
+
+            _htmlNode = htmlDocument.DocumentNode;
         }
 
         public BeautifulNode SelectNode(string xpath)
         {
             if (xpath.EndsWith("option"))
             {
-                return new BeautifulNode(_htmlDocument.DocumentNode.SelectSingleNode(xpath).NextSibling);
+                return new BeautifulNode(_htmlNode.SelectSingleNode(xpath).NextSibling);
             }
 
-            return new BeautifulNode(_htmlDocument.DocumentNode.SelectSingleNode(xpath));
+            return new BeautifulNode(_htmlNode.SelectSingleNode(xpath));
         }
 
         public IEnumerable<BeautifulNode> SelectNodes(string xpath)
         {
+            var nodes = _htmlNode.SelectNodes(xpath);
+            if (!nodes.Any()) return new List<BeautifulNode>();
+
             if (xpath.EndsWith("option"))
             {
-                return _htmlDocument.DocumentNode.SelectNodes(xpath).Select(n => new BeautifulNode(n.NextSibling));
+                return nodes.Select(n => new BeautifulNode(n.NextSibling));
             }
 
-            return _htmlDocument.DocumentNode.SelectNodes(xpath).Select(n => new BeautifulNode(n));
+            return nodes.Select(n => new BeautifulNode(n));
         }
     }
 }
